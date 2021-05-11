250ad4c1613b5ed7ba6c23e8f42c0f8d5ee23f48
cihangulx <emrekocamman47@gmail.com>
2021-05-11 14:45:02 +0300

    First commit for base classes

---
+++ base/src/main/java/com/emre/base/ToastUtils.java
@@ -0,0 +1,43 @@
+package com.emre.base;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils {
+
+    private static Toast toast;
+
+    private ToastUtils(){
+        throw new AssertionError();
+    }
+
+    public static void showToast(Context context, int resId){
+        showToast(context,context.getResources().getString(resId));
+    }
+
+    public static void showToast(Context context, int resId, int duration){
+        showToast(context,context.getResources().getString(resId),duration);
+    }
+
+    public static void showToast(Context context, CharSequence text){
+        showToast(context,text, Toast.LENGTH_SHORT);
+    }
+
+    public static void showToast(Context context, String text, int duration, Object...args){
+        showToast(context, String.format(text,args),duration);
+    }
+
+    public static void showToast(Context context, CharSequence text, int duration){
+        if(toast == null){
+            synchronized (ToastUtils.class){
+                if(toast == null){
+                    toast = Toast.makeText(context,text,duration);
+                }
+            }
+        }else{
+            toast.setText(text);
+            toast.setDuration(duration);
+        }
+        toast.show();
+    }
+}
